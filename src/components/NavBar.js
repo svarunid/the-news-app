cc77e59c4a37aeff325c6c38bfbf34ac5421374e
Arun S V <svarunid@gmail.com>
2022-03-31 17:27:10 +0530

    InitalCommit

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="bg-gray-900 p-6">
+      <button className="font-bold text-white text-lg">The News App</button>
+    </div>
+  );
+}
